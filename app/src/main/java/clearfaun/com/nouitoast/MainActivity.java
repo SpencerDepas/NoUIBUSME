b494b5d535ae6b798391cfc6447116ceb5b1df28
SD <spencerdepas@gmail.com>
2015-02-01 15:17:54 -0500

    Changed to a serviceIntent.

--- app/src/main/java/clearfaun/com/nouitoast/MainActivity.java
+++ app/src/main/java/clearfaun/com/nouitoast/MainActivity.java
@@ -34,15 +34,16 @@ public class MainActivity extends Activity {
     public static Activity activity1;
     private static LocationManager locationManager;
     private static String provider;
-    static EditText editText;
-    static EditText editTextTwo;
-    static EditText editTextThree;
 
     final static public String API_KEY = "05a5c2c8-432a-47bd-8f50-ece9382b4b28";
 
 
     static String testLat = "40.6455520";
     static String testLng = "-73.9829084";
+
+    public static BusInfo busInfo = new BusInfo();
+    static Geocoder geocoder;
+    static Context mContext;
 
 
     static double latatude;
@@ -53,77 +54,30 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+        LocationManager lService = (LocationManager) getSystemService(LOCATION_SERVICE);
+        final boolean enabled = lService.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
         mContext = getApplicationContext();
 
-
-        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
-        final boolean enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        provider = locationManager.getBestProvider(criteria, true);
-
-        LocationListener locationListener = new MyLocationListener();
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-
-
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-
         if (!enabled) {
+
             Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
             startActivity(intent);
-        }
 
+        }else {
 
-        int accuracy = (int) location.getAccuracy();
-        long time= System.currentTimeMillis();
-        Log.i("MyActivity12", "before while accuracy loop — accuracy = " + accuracy);
-        while (accuracy > 10) {
+            Intent service = new Intent(this, Service.class);
+            startService(service);
 
-            Log.i("MyActivity12", "in while accuracy loop — accuracy = " + accuracy);
-            accuracy = (int) location.getAccuracy();
-            if(time >= time + 5000){
-                break;
-            }
-            //do nothing!
-            //need to add a case where its not acurate.
-        }
-        Log.i("MyActivity12", "after while accuracy loop — accuracy = " + accuracy);
-
-
-        // Initialize the location fields
-        if (location != null) {
-
-                onLocationChanged(location);
 
         }
 
-
-
-        GpsToAddress task = new GpsToAddress();
-        task.execute();
-        Log.i("MyActivity12", "after GpsToAddress = " + accuracy);
 
     }
 
 
 
-    public static BusInfo busInfo = new BusInfo();
-    static Geocoder geocoder;
-
-
-
-    public static void onLocationChanged(Location location) {
-        latatude =  location.getLatitude();
-        longitude =  location.getLongitude();
-        //editText.setText(String.valueOf(latatude) + "\n" + String.valueOf(longitude));
-
-    }
 
     @Override
     public void onResume(){
@@ -131,8 +85,6 @@ public class MainActivity extends Activity {
         // put your code here...
         finish();
     }
-
-    static Context mContext;
 
 
 
@@ -143,8 +95,6 @@ public class MainActivity extends Activity {
         Toast toast = Toast.makeText(mContext, string, Toast.LENGTH_LONG);
         toast.show();
     }
-
-
 
 
 
---
+++ app/src/main/java/clearfaun/com/nouitoast/Service.java
@@ -0,0 +1,94 @@
+package clearfaun.com.nouitoast;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.EditText;
+
+/**
+ * Created by spencer on 2/1/2015.
+ */
+public class Service extends IntentService {
+
+
+    private static LocationManager locationManager;
+    private static String provider;
+
+
+
+    public Service() {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent){
+        // Handle events on worker thread here
+
+
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        provider = locationManager.getBestProvider(criteria, true);
+
+        LocationListener locationListener = new MyLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
+
+
+
+        int accuracy = (int) location.getAccuracy();
+        long time= System.currentTimeMillis();
+        Log.i("MyActivity12", "before while accuracy loop — accuracy = " + accuracy);
+        while (accuracy > 10) {
+
+            Log.i("MyActivity12", "in while accuracy loop — accuracy = " + accuracy);
+            accuracy = (int) location.getAccuracy();
+
+
+            if(time >= time + 5000){
+                break;
+            }
+
+        }
+        Log.i("MyActivity12", "after while accuracy loop — accuracy = " + accuracy);
+
+
+        // Initialize the location fields
+        if (location != null) {
+
+            onLocationChanged(location);
+
+        }
+
+
+
+        GpsToAddress task = new GpsToAddress();
+        task.execute();
+        Log.i("MyActivity12", "after GpsToAddress = " + accuracy);
+
+
+    }
+
+
+    public static void onLocationChanged(Location location) {
+        MainActivity.latatude =  location.getLatitude();
+        MainActivity.longitude =  location.getLongitude();
+        //editText.setText(String.valueOf(latatude) + "\n" + String.valueOf(longitude));
+
+    }
+
+}
+
