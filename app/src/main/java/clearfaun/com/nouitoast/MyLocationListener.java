02a544a381ee8c5b1893b4b4e6375877db9055df
SD <spencerdepas@gmail.com>
2015-02-07 17:13:59 -0500

    Made a replacement locationlistner, now a call is made only when x accurate. Need to test.

--- app/src/main/java/clearfaun/com/nouitoast/MyLocationListener.java
+++ app/src/main/java/clearfaun/com/nouitoast/MyLocationListener.java
@@ -10,51 +10,50 @@ import android.util.Log;
  */
 public class MyLocationListener implements LocationListener {
 
+    int accuracy;
+
+
     @Override
-    public void onLocationChanged(Location locFromGps) {
+    public void onLocationChanged(Location location) {
 
-      /*  int accuracy = (int) locFromGps.getAccuracy();
-        long time = System.currentTimeMillis();
+        accuracy = (int) location.getAccuracy();
 
-        Log.i("MyActivity12", "MyLocationListener: DO I WORK " + accuracy);
-        if (locFromGps.getAccuracy() < 10 ) {
+        Log.i("MyLocationListener", "IN  onLocationChanged !!!!!!!!!!!!" + accuracy);
 
+        if(accuracy <= 40) {
 
-
-            Log.i("MyActivity12", "MyLocationListener: DOES MY ACCURACY CHANGE? " + accuracy);
+            MainActivity.latatude = location.getLatitude();
+            MainActivity.longitude = location.getLongitude();
 
             GpsToAddress task = new GpsToAddress();
             task.execute();
-            Log.i("MyActivity12", "after GpsToAddress = " + accuracy);
-
-        }*/
+            Log.i("MyLocationListener", "MyLocationListener in " + accuracy);
 
 
-
+        }
 
 
         // called when the listener is notified with a location update from the GPS
     }
-    public static void whenLocationChanged(Location location) {
-        MainActivity.latatude =  location.getLatitude();
-        MainActivity.longitude =  location.getLongitude();
-        //editText.setText(String.valueOf(latatude) + "\n" + String.valueOf(longitude));
 
-    }
 
 
     @Override
     public void onProviderDisabled(String provider) {
         // called when the GPS provider is turned off (user turning off the GPS on the phone)
+        Log.i("MyLocationListener", "onProviderDisabled " + accuracy);
     }
 
     @Override
     public void onProviderEnabled(String provider) {
         // called when the GPS provider is turned on (user turning on the GPS on the phone)
+        Log.i("MyLocationListener", "onProviderEnabled  " + accuracy);
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
         // called when the status of the GPS provider changes
+        Log.i("MyLocationListener", "onStatusChanged" + accuracy);
+
     }
 }
--- app/src/main/java/clearfaun/com/nouitoast/Service.java
+++ app/src/main/java/clearfaun/com/nouitoast/Service.java
@@ -1,29 +1,36 @@
 package clearfaun.com.nouitoast;
 
 import android.app.IntentService;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.util.Log;
 
 
 /**
  * Created by spencer on 2/1/2015.
  */
-public class Service extends IntentService {
+public class Service extends IntentService{
 
 
-    private static LocationManager locationManager;
+    static LocationManager locationManager;
     private static String provider;
-
+    int accuracy;
+    LocationListener locationListener;
 
 
     public Service() {
         super("MyIntentService");
     }
+
+
 
     @Override
     protected void onHandleIntent(Intent intent){
@@ -32,61 +39,115 @@ public class Service extends IntentService {
         Log.i("MyActivity12", "onHandleIntent " );
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        provider = locationManager.getBestProvider(criteria, true);
-
-        LocationListener locationListener = new MyLocationListener();
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
 
+        if (locationManager != null) {
 
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-
+            Log.i("MyActivity12", "onHandleIntent locationManager != null" );
 
 
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            locationManager.getBestProvider(criteria, true);
 
-        int accuracy = (int) location.getAccuracy();
-        long time = System.currentTimeMillis();
-        Log.i("MyActivity12", "before while accuracy loop — accuracy = " + accuracy);
-        while (accuracy > 10 ) {
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            MyLocationListener locationListener = new MyLocationListener();
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
 
-            accuracy = (int) location.getAccuracy();
 
-            if(System.currentTimeMillis() >= time + 5000){
-                break;
+
+
+
+
+            Log.i("MyActivity12", "before if getAccuracy <= " + (int)location.getAccuracy());
+
+
+            if((int)location.getAccuracy() <= 10){
+
+                MainActivity.latatude = location.getLatitude();
+                MainActivity.longitude = location.getLongitude();
+
+
+                GpsToAddress task = new GpsToAddress();
+                task.execute();
+                Log.i("MyActivity12", "in if GpsToAddress = " + accuracy);
+
+            }else{
+                Log.i("MyActivity12", "entering  untilAccurate" + accuracy);
+                untilAccurate();
             }
 
+
+            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
+            /*accuracy = (int) location.getAccuracy();
+            long time = System.currentTimeMillis();
+            Log.i("MyActivity12", "before while accuracy loop — accuracy = " + accuracy);
+            while (accuracy > 10) {
+
+
+                accuracy = (int) location.getAccuracy();
+
+                if (System.currentTimeMillis() >= time + 5000) {
+                    break;
+                }
+
+            }*/
+            //Log.i("MyActivity12", "after while accuracy loop — accuracy = " + accuracy);
+
+
         }
-        Log.i("MyActivity12", "after while accuracy loop — accuracy = " + accuracy);
-
-
-        // Initialize the location fields
-        if (location != null) {
-
-            onLocationChanged(location);
-
-        }
 
 
 
-        GpsToAddress task = new GpsToAddress();
-        task.execute();
-        Log.i("MyActivity12", "after GpsToAddress = " + accuracy);
+
+
+
 
 
     }
 
 
-    public static void onLocationChanged(Location location) {
-        MainActivity.latatude =  location.getLatitude();
-        MainActivity.longitude =  location.getLongitude();
-        //editText.setText(String.valueOf(latatude) + "\n" + String.valueOf(longitude));
+    public void untilAccurate() {
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Log.i("MyActivity12", "in  untilAccurate accuracy is " + (int)location.getAccuracy());
+        MyLocationListener locationListener = new MyLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
+
+        if((int)location.getAccuracy() >= 15){
+            Log.i("MyActivity12", "in  the if " + (int)location.getAccuracy());
+
+            long time = System.currentTimeMillis();
+            while(true){
+                if (System.currentTimeMillis() >= time + 3000) {
+                    break;
+                }
+            }
+
+            untilAccurate();
+        }else{
+
+            MainActivity.latatude = location.getLatitude();
+            MainActivity.longitude = location.getLongitude();
+
+
+            GpsToAddress task = new GpsToAddress();
+            task.execute();
+            Log.i("MyActivity12", "in untilAccurate otw to  GpsToAddress" + (int)location.getAccuracy());
+
+        }
+        Log.i("MyActivity12", "in untilAccurate outside else" + (int)location.getAccuracy());
 
     }
+
+
+
+
 
 }
 
