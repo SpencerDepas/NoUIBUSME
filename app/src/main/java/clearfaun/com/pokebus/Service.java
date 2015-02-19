67559e40bc812b233c725d168b83e60697e29c61
SD <spencerdepas@gmail.com>
2015-02-19 13:53:12 -0500

    Fixed until accurate, needs testing.

--- app/src/main/java/clearfaun/com/pokebus/Service.java
+++ app/src/main/java/clearfaun/com/pokebus/Service.java
@@ -8,7 +8,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.util.Log;
-import android.widget.Toast;
+
 
 import com.segment.analytics.Analytics;
 import com.segment.analytics.Properties;
@@ -37,14 +37,14 @@ public class Service extends IntentService{
 
 
 
-            Log.i("MyActivity12", "Service onHandleIntent " );
+            Log.i("MyService", "Service onHandleIntent " );
 
             locationManager = (LocationManager) MainActivity.mContext.getSystemService(Context.LOCATION_SERVICE);
 
 
             if (locationManager != null) {
 
-                Log.i("MyActivity12", "onHandleIntent locationManager != null");
+                Log.i("MyService", "onHandleIntent locationManager != null");
 
 
 
@@ -64,9 +64,9 @@ public class Service extends IntentService{
 
 
 
-                        Log.i("MyActivity12", "before if getAccuracy test:  " + (int) location.getAccuracy());
+                        Log.i("MyService", "before if getAccuracy test:  " + (int) location.getAccuracy());
 
-
+                        //this sets first test for desired accuracy
                         if ((int) location.getAccuracy() <= 10 && (int) location.getAccuracy() != 0) {
 
                             Analytics.with(this).track("Accuracy", new Properties()
@@ -78,22 +78,22 @@ public class Service extends IntentService{
 
                             GpsToAddress task = new GpsToAddress();
                             task.execute();
-                            Log.i("MyActivity12", "in if GpsToAddress <= " + (int) location.getAccuracy());
+                            Log.i("MyService", "in if GpsToAddress <= " + (int) location.getAccuracy());
 
                         } else {
-                            Log.i("MyActivity12", "entering  untilAccurate " + (int) location.getAccuracy());
+                            Log.i("MyService", "entering  untilAccurate " + (int) location.getAccuracy());
                             untilAccurate();
                         }
 
 
                     }else {
-                        Log.i("MyActivity12", "!location.hasAccuracy()");
+                        Log.i("MyService", "!location.hasAccuracy()");
                     }
 
                 }else{
 
                     new ToastMessageTask().execute("Location not available");
-                    Log.i("MyActivity12", "location == null " );
+                    Log.i("MyService", "location == null " );
                 }
             }
 
@@ -105,37 +105,39 @@ public class Service extends IntentService{
         public void untilAccurate() {
 
             Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            Log.i("MyActivity12", "in  untilAccurate accuracy is :" + (int)location.getAccuracy() + "  trackUntilAccurateLoops: " + trackUntilAccurateLoops);
+            Log.i("MyServiceUntilAccurate", "in  untilAccurate accuracy is :" + (int)location.getAccuracy() + "  trackUntilAccurateLoops: " + trackUntilAccurateLoops);
             MyLocationListener locationListener = new MyLocationListener();
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
 
             if(trackUntilAccurateLoops < 3) {
-                Log.i("MyActivity12", "in  trackUntilAccurateLoops  :  trackUntilAccurateLoops: " + trackUntilAccurateLoops );
+                Log.i("MyServiceUntilAccurate", "in  trackUntilAccurateLoops  :  trackUntilAccurateLoops: " + trackUntilAccurateLoops );
                 if ((int) location.getAccuracy() >= 15 && (int) location.getAccuracy() != 0) {
 
-                    Log.i("MyActivity12", "location.getAccuracy() >= 15  Accuracy: " + (int) location.getAccuracy());
-                    if (trackUntilAccurateLoops != 0) {
-                        Analytics.with(this).track("untilAccurate", new Properties()
-                                .putValue("UntilAccurateLoops", trackUntilAccurateLoops + ""));
-                    }
+
+
 
 
                     long time = System.currentTimeMillis();
+                    Log.i("MyServiceUntilAccurate", "location.getAccuracy() >= 15  Accuracy: " + (int) location.getAccuracy());
                     while (true) {
                         if (System.currentTimeMillis() >= time + 3000) {
                             break;
                         }
                     }
-                    Log.i("MyActivity12", "after while loop: " + trackUntilAccurateLoops);
+                    Log.i("MyServiceUntilAccurate", "after while loop: " + trackUntilAccurateLoops);
                     trackUntilAccurateLoops++;
                     untilAccurate();
                 } else {
 
+                    if (trackUntilAccurateLoops != 0) {
+                        Analytics.with(this).track("untilAccurate", new Properties()
+                                .putValue("UntilAccurateLoops", trackUntilAccurateLoops + ""));
+                    }
 
                     Analytics.with(this).track("untilAccurate", new Properties()
                             .putValue("Accuracy", (int) location.getAccuracy() + ""));
-                    Log.i("MyActivity12", "in untilAccurate otw to  GpsToAddress" + (int) location.getAccuracy());
+                    Log.i("MyServiceUntilAccurate", "in untilAccurate otw to  GpsToAddress" + (int) location.getAccuracy());
                     MainActivity.latatude = location.getLatitude();
                     MainActivity.longitude = location.getLongitude();
 
@@ -147,7 +149,11 @@ public class Service extends IntentService{
                 }
 
             }else{
-                Log.i("MyActivity12", "Accurate GPS reading can not be given "  + (int) location.getAccuracy());
+                if (trackUntilAccurateLoops != 0) {
+                    Analytics.with(this).track("untilAccurate", new Properties()
+                            .putValue("UntilAccurateLoops", trackUntilAccurateLoops + ""));
+                }
+                Log.i("MyServiceUntilAccurate", "Accurate GPS reading can not be given "  + (int) location.getAccuracy());
                 new ToastMessageTask().execute("Accurate GPS reading can not be given");
             }
 
